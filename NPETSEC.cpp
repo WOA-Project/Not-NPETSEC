d8ae22c5a3f22e570fd9b8c9569172bcb52c8ee5
Gus <gus33000@live.fr>
2019-05-11 19:34:37 +0200

    Updated repository with latest source

--- NPETSEC.cpp
+++ NPETSEC.cpp
@@ -1,581 +1,123 @@
+/*
+   NPETSEC - Modem Subscription activation bootstrapper
+*/
+
 #include "pch.h"
 #include "NPETSEC.h"
-
-#include <iostream>
 #include <fstream>
-#include <sstream>
-
-HANDLE hDLL;
 
 using namespace std;
 
-ofstream logFile;
+/*
+   Activates the Modem subscription
 
-LPNPETSEC_INIT Init;
-LPNPETSEC_UNLOCKSIMLOCK UnlockSimlock;
-LPNPETSEC_GETSIMLOCKSTATUS GetSimlockStatus;
-LPNPETSEC_DEINIT Deinit;
+   We launch the activation tool in order to activate the modem subscription.
+   We also enable extra logging if the user requested us to log items into a text file.
+*/
 
-std::string ToString(__int64 obj)
+__int64 Activate()
 {
-	std::stringstream stream;
-	stream << obj;
-	std::string strValue(stream.str());
-	return strValue;
-}
+	// Control logging behind a registry flag
+	HKEY hKey;
+	LSTATUS nResult = ::RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"SOFTWARE\\OEM\\NPETSEC", 0, KEY_READ | KEY_WOW64_64KEY, &hKey);
 
-__int64 NPETSEC_Init(__int64 val1)
-{
-	// Initialize stubbed library entry points
-	hDLL = LoadLibrary(L"NPETSEC2.DLL");
+	DWORD dwData;
+	DWORD cbData = sizeof(DWORD);
 
-	LPNPETSEC_INIT Init = (LPNPETSEC_INIT)GetProcAddress((HMODULE)hDLL, "NPETSEC_Init");
+	bool Logging = FALSE;
+
+	if (nResult == ERROR_SUCCESS)
+	{
+		nResult = ::RegQueryValueEx(hKey, L"EnableLogging", NULL, NULL, (LPBYTE)&dwData, &cbData);
+		RegCloseKey(hKey);
+
+		if (nResult == ERROR_SUCCESS)
+			if (dwData == 1)
+				Logging = TRUE;
+	}
+
+	ofstream logFile;
 
 	// Proceed with initialization
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_Init";
-	logFile << "\n";
-	logFile << ToString(val1);
-	logFile << "\n";
 
-	__int64 ret = Init(val1);
-
-	logFile << "< NPETSEC_Init";
-	logFile << "\n";
-	logFile << ToString(ret);
-
-	logFile << "\n";
-	logFile << "Bypassing error by returning 0";
-
-	logFile << "\n";
-
-	return 0;//ret;
-}
-
-__int64 NPETSEC_DppItemSize(unsigned int itemId, int* bytecount)
-{
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_DppItemSize";
-	logFile << "\n";
-	logFile << "Item ID: ";
-	logFile << itemId;
-	logFile << "\n";
-
-	string filePath;
-
-	switch (itemId)
+	if (Logging)
 	{
-		case 0:
-		{
-			filePath = "\\Microsoft\\Microsoft.PVK";
-			break;
-		}
-		case 1:
-		{
-			filePath = "\\QCOM\\BT.Provision";
-			break;
-		}
-		case 2:
-		{
-			filePath = "\\QCOM\\WLAN.Provision";
-			break;
-		}
-		case 3:
-		{
-			filePath = "\\MMO\\CoverColor.txt";
-			break;
-		}
-		case 4:
-		{
-			filePath = "\\MMO\\customer_nvi_log.txt";
-			break;
-		}
-		case 5:
-		{
-			filePath = "\\MMO\\simlock\\sign";
-			break;
-		}
-		case 6:
-		{
-			filePath = "\\MMO\\simlock\\keys";
-			break;
-		}
-		case 7:
-		{
-			filePath = "\\MMO\\simlock\\lock";
-			break;
-		}
-		case 8:
-		{
-			filePath = "\\MMO\\product.dat";
-			break;
-		}
-		case 9:
-		{
-			filePath = "\\MMO\\certs\\hwc";
-			break;
-		}
-		case 10:
-		{
-			filePath = "\\MMO\\certs\\ccc";
-			break;
-		}
-		case 11:
-		{
-			filePath = "\\MMO\\certs\\npc";
-			break;
-		}
-		case 12:
-		{
-			filePath = "\\MMO\\certs\\rdc";
-			break;
-		}
-		case 13:
-		{
-			filePath = "\\MMO\\certs\\devcert.bin";
-			break;
-		}
-		case 14:
-		{
-			filePath = "\\MMO\\certs\\mirlink.bin";
-			break;
-		}
-		case 15:
-		{
-			filePath = "\\MMO\\RegScreen\\imagelabel_dark.png";
-			break;
-		}
-		case 16:
-		{
-			filePath = "\\MMO\\RegScreen\\imagelabel_light.png";
-			break;
-		}
-		case 17:
-		{
-			filePath = "\\MMO\\simlock\\unlock.bin";
-			break;
-		}
-		case 18:
-		{
-			filePath = "\\MMO\\ssdhash.bin";
-			break;
-		}
-		case 19:
-		{
-			filePath = "\\MMO\\certs\\label_data.bin";
-			break;
-		}
-		case 20:
-		{
-			filePath = "\\MMO\\RegScreen\\coo.txt";
-			break;
-		}
-		case 21:
-		{
-			filePath = "\\MMO\\imageimeibarcode.png";
-			break;
-		}
-		case 22:
-		{
-			filePath = "\\MMO\\fsghash.bin";
-			break;
-		}
-		case 23:
-		{
-			filePath = "\\MMO\\Label\\panel.ver";
-			break;
-		}
-		case 24:
-		{
-			filePath = "\\MMO\\Label\\label.ver";
-			break;
-		}
-		case 25:
-		{
-			filePath = "\\MMO\\testfile.txt";
-			break;
-		}
-		case 26:
-		{
-			filePath = "\\MMO\\testfilerestricted.txt";
-			break;
-		}
-		case 27:
-		{
-			filePath = "\\MMO\\Label\\panelmdcl.ver";
-			break;
-		}
-		case 28:
-		{
-			filePath = "\\MMO\\Label\\labelmdcl.ver";
-			break;
-		}
-		case 29:
-		{
-			filePath = "\\MMO\\simlock\\sign2";
-			break;
-		}
+		logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
+		logFile << "> Activating RIL Subscription" << "\n";
 	}
 
-	logFile << "File name from ID: ";
-	logFile << filePath;
-	logFile << " -> ";
-	logFile << "C:\\DPP" + filePath;
-	logFile << "\n";
-
-	__int64 ret = 0;
-
-	try
+	char psBuffer[128];
+	FILE * iopipe;
+	if ((iopipe = _popen("\\Windows\\System32\\RilServiceActivation.exe", "r")) == NULL)
 	{
-		ifstream ifs("C:\\DPP" + filePath, ios::binary | ios::in);
-		ifstream::pos_type pos = ifs.tellg();
-
-		ifs.seekg(0, ios::beg);
-		bytecount = ifs.gcount();
-
-		ifs.close();
-	}
-	catch (int e)
-	{
-		logFile << "Exception: " << e << '\n';
-		ret = e;
+		if (Logging)
+		{
+			logFile << "< Cannot find the RIL Subscription activation utility. Leaving..." << "\n";
+			logFile.close();
+		}
+		return 1;
 	}
 
-	logFile << "< NPETSEC_DppItemSize";
-	logFile << "\n";
-	logFile << ToString(ret);
-	logFile << "\n";
-
-	logFile.close();
-
-	return ret;
-}
-
-__int64 NPETSEC_UnlockSimlock(unsigned int val1, __int64 val2)
-{
-	LPNPETSEC_UNLOCKSIMLOCK UnlockSimlock = (LPNPETSEC_UNLOCKSIMLOCK)GetProcAddress((HMODULE)hDLL, "NPETSEC_UnlockSimlock");
-
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_UnlockSimlock";
-	logFile << "\n";
-	logFile << val1;
-	logFile << "\n";
-	logFile << ToString(val2);
-	logFile << "\n";
-
-	__int64 ret = UnlockSimlock(val1, val2);
-
-	logFile << "< NPETSEC_UnlockSimlock";
-	logFile << "\n";
-	logFile << ToString(ret);
-
-	logFile << "\n";
-	logFile << "Bypassing error by returning 0";
-
-	logFile << "\n";
-	logFile.close();
-
-	return 0;//ret;
-}
-
-__int64 NPETSEC_GetSimlockStatus(__int64 val1)
-{
-	LPNPETSEC_GETSIMLOCKSTATUS GetSimlockStatus = (LPNPETSEC_GETSIMLOCKSTATUS)GetProcAddress((HMODULE)hDLL, "NPETSEC_GetSimlockStatus");
-
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_GetSimlockStatus";
-	logFile << "\n";
-	logFile << ToString(val1);
-	logFile << "\n";
-
-	__int64 ret =  GetSimlockStatus(val1);
-
-	logFile << "< NPETSEC_GetSimlockStatus";
-	logFile << "\n";
-	logFile << ToString(ret);
-
-	logFile << "\n";
-	logFile << "Bypassing error by returning 0";
-
-	logFile << "\n";
-	logFile.close();
-
-	return 0;//ret;
-}
-
-__int64 NPETSEC_DppItemRead(unsigned int itemId, unsigned int loggingflag, char* outputBuffer, unsigned int bytecount)
-{
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-
-	logFile << "> NPETSEC_DppItemRead";
-	logFile << "\n";
-	logFile << "Item ID: ";
-	logFile << itemId;
-	logFile << "\n";
-	logFile << "Logging flag: ";
-	logFile << loggingflag;
-	logFile << "\n";
-	logFile << "Byte count: ";
-	logFile << bytecount;
-	logFile << "\n";
-
-	string filePath;
-
-	switch (itemId)
+	while (!feof(iopipe))
 	{
-		case 0:
-		{
-			filePath = "\\Microsoft\\Microsoft.PVK";
-			break;
-		}
-		case 1:
-		{
-			filePath = "\\QCOM\\BT.Provision";
-			break;
-		}
-		case 2:
-		{
-			filePath = "\\QCOM\\WLAN.Provision";
-			break;
-		}
-		case 3:
-		{
-			filePath = "\\MMO\\CoverColor.txt";
-			break;
-		}
-		case 4:
-		{
-			filePath = "\\MMO\\customer_nvi_log.txt";
-			break;
-		}
-		case 5:
-		{
-			filePath = "\\MMO\\simlock\\sign";
-			break;
-		}
-		case 6:
-		{
-			filePath = "\\MMO\\simlock\\keys";
-			break;
-		}
-		case 7:
-		{
-			filePath = "\\MMO\\simlock\\lock";
-			break;
-		}
-		case 8:
-		{
-			filePath = "\\MMO\\product.dat";
-			break;
-		}
-		case 9:
-		{
-			filePath = "\\MMO\\certs\\hwc";
-			break;
-		}
-		case 10:
-		{
-			filePath = "\\MMO\\certs\\ccc";
-			break;
-		}
-		case 11:
-		{
-			filePath = "\\MMO\\certs\\npc";
-			break;
-		}
-		case 12:
-		{
-			filePath = "\\MMO\\certs\\rdc";
-			break;
-		}
-		case 13:
-		{
-			filePath = "\\MMO\\certs\\devcert.bin";
-			break;
-		}
-		case 14:
-		{
-			filePath = "\\MMO\\certs\\mirlink.bin";
-			break;
-		}
-		case 15:
-		{
-			filePath = "\\MMO\\RegScreen\\imagelabel_dark.png";
-			break;
-		}
-		case 16:
-		{
-			filePath = "\\MMO\\RegScreen\\imagelabel_light.png";
-			break;
-		}
-		case 17:
-		{
-			filePath = "\\MMO\\simlock\\unlock.bin";
-			break;
-		}
-		case 18:
-		{
-			filePath = "\\MMO\\ssdhash.bin";
-			break;
-		}
-		case 19:
-		{
-			filePath = "\\MMO\\certs\\label_data.bin";
-			break;
-		}
-		case 20:
-		{
-			filePath = "\\MMO\\RegScreen\\coo.txt";
-			break;
-		}
-		case 21:
-		{
-			filePath = "\\MMO\\imageimeibarcode.png";
-			break;
-		}
-		case 22:
-		{
-			filePath = "\\MMO\\fsghash.bin";
-			break;
-		}
-		case 23:
-		{
-			filePath = "\\MMO\\Label\\panel.ver";
-			break;
-		}
-		case 24:
-		{
-			filePath = "\\MMO\\Label\\label.ver";
-			break;
-		}
-		case 25:
-		{
-			filePath = "\\MMO\\testfile.txt";
-			break;
-		}
-		case 26:
-		{
-			filePath = "\\MMO\\testfilerestricted.txt";
-			break;
-		}
-		case 27:
-		{
-			filePath = "\\MMO\\Label\\panelmdcl.ver";
-			break;
-		}
-		case 28:
-		{
-			filePath = "\\MMO\\Label\\labelmdcl.ver";
-			break;
-		}
-		case 29:
-		{
-			filePath = "\\MMO\\simlock\\sign2";
-			break;
-		}
+		if (fgets(psBuffer, 128, iopipe) != NULL)
+			if (Logging)
+				logFile << "- " << psBuffer;
 	}
 
-	logFile << "File name from ID: ";
-	logFile << filePath;
-	logFile << " -> ";
-	logFile << "C:\\DPP" + filePath;
-	logFile << "\n";
-
-	logFile << "Reading start";
-	logFile << "\n";
-
-	__int64 ret = 0;
-
-	try
+	if (Logging)
 	{
-		ifstream ifs("C:\\DPP" + filePath, ios::binary | ios::in);
-		ifstream::pos_type pos = ifs.tellg();
-
-		ifs.seekg(0, ios::beg);
-		ifs.read(outputBuffer, bytecount);
-		ifs.close();
+		logFile << "< Subscription activated" << "\n";
+		logFile.close();
 	}
-	catch (int e)
-	{
-		logFile << "Exception: " << e << '\n';
-		ret = e;
-	}
-
-	logFile << "Reading end";
-	logFile << "\n";
-
-	logFile << "< NPETSEC_DppItemRead";
-	logFile << "\n";
-	logFile << "Returning 0";
-	logFile << "\n";
-
-	logFile.close();
-
-	return ret;
-}
-
-__int64 NPETSEC_ValidateMultiSim(char* outputBuffer, char* Buffer2)
-{
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_ValidateMultiSim";
-	logFile << "\n";
-
-	logFile << sizeof(*outputBuffer) << "\n";
-	logFile << sizeof(*Buffer2) << "\n";
-
-	char  outputBuffer2[23];
-
-	char data[] = { 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x01, 0x00, 0x02, 0x01 };
-
-	/*outputBuffer2[0] = 0x00;
-	outputBuffer2[1] = 0x00;
-	outputBuffer2[2] = 0x00;
-	outputBuffer2[3] = 0x00;
-	outputBuffer2[4] = 0x01;
-	outputBuffer2[5] = 0x00;
-	outputBuffer2[6] = 0x00;
-	outputBuffer2[7] = 0x00;
-	outputBuffer2[8] = 0x01;
-	outputBuffer2[9] = 0x00;
-	outputBuffer2[10] = 0x00;
-	outputBuffer2[11] = 0x00;
-	outputBuffer2[12] = 0x01;
-	outputBuffer2[13] = 0x00;
-	outputBuffer2[14] = 0x00;
-	outputBuffer2[15] = 0x00;
-	outputBuffer2[16] = 0x00;
-	outputBuffer2[17] = 0x01;
-	outputBuffer2[18] = 0x01;
-	outputBuffer2[19] = 0x00;
-	outputBuffer2[20] = 0x01;
-	outputBuffer2[21] = 0x00;
-	outputBuffer2[22] = 0x02;
-	outputBuffer2[23] = 0x01;*/
-
-	Buffer2 = data;
-
-	logFile << sizeof(Buffer2) << "\n";
-	logFile << sizeof(*Buffer2) << "\n";
-
-	logFile << "< NPETSEC_ValidateMultiSim";
-	logFile << "\n";
-
-	logFile.close();
 
 	return 0;
 }
 
+/*
+   We do not call activate for every function because the Modem might not be ready when APPerso requests us to Initialize
+   Or read a DPP item.
+
+   Only activate the subscription when we're called from ValidateMultiSim.
+
+   If no Activation is required, we will never be called.
+*/
+
+__int64 NPETSEC_Init(__int64 val1) 
+{
+	return 0;
+}
+
+__int64 NPETSEC_DppItemSize(unsigned int val1, int* val2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_UnlockSimlock(unsigned int val1, __int64 val2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetSimlockStatus(__int64 val1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_DppItemRead(unsigned int a1, unsigned int a2, __int64 a3, unsigned int a4)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ValidateMultiSim(__int64 a1, __int64 a2)
+{
+	//
+	// When APPerso calls this function, the Modem is ready and is waiting to be activated
+	// Activate the Modem subscription
+	//
+	return Activate();
+}
+
 void    NPETSEC_Deinit()
 {
-	LPNPETSEC_DEINIT Deinit = (LPNPETSEC_DEINIT)GetProcAddress((HMODULE)hDLL, "NPETSEC_Deinit");
-
-	logFile.open("C:\\RIL_NPETSEC.txt", std::ofstream::out | std::ofstream::app);
-	logFile << "> NPETSEC_Deinit";
-	logFile << "\n";
-
-	Deinit();
-
-	logFile << "< NPETSEC_Deinit";
-	logFile << "\n";
-	logFile.close();
+	
 }
--- NPETSEC.h
+++ NPETSEC.h
@@ -8,33 +8,9 @@
 #endif
 
 extern "C" NPETSEC_API __int64 NPETSEC_Init(__int64 val1);
-extern "C" NPETSEC_API __int64 NPETSEC_DppItemSize(unsigned int val1, int* val2);
+extern "C" NPETSEC_API __int64 NPETSEC_DppItemSize(unsigned int val1, int *val2);
 extern "C" NPETSEC_API __int64 NPETSEC_UnlockSimlock(unsigned int val1, __int64 val2);
 extern "C" NPETSEC_API __int64 NPETSEC_GetSimlockStatus(__int64 val1);
-extern "C" NPETSEC_API __int64 NPETSEC_DppItemRead(unsigned int itemId, unsigned int loggingflag, char* outputBuffer, unsigned int bytecount);
-extern "C" NPETSEC_API __int64 NPETSEC_ValidateMultiSim(char* outputBuffer, char* outputBuffer2);
+extern "C" NPETSEC_API __int64 NPETSEC_DppItemRead(unsigned int a1, unsigned int a2, __int64 a3, unsigned int a4);
+extern "C" NPETSEC_API __int64 NPETSEC_ValidateMultiSim(__int64 a1, __int64 a2);
 extern "C" NPETSEC_API void    NPETSEC_Deinit();
-
-typedef __int64(STDAPICALLTYPE NPETSEC_INIT)
-(
-	IN __int64				val1
-);
-typedef NPETSEC_INIT FAR* LPNPETSEC_INIT;
-
-typedef __int64(STDAPICALLTYPE NPETSEC_UNLOCKSIMLOCK)
-(
-	IN unsigned int val1, IN __int64 val2
-);
-typedef NPETSEC_UNLOCKSIMLOCK FAR* LPNPETSEC_UNLOCKSIMLOCK;
-
-typedef __int64(STDAPICALLTYPE NPETSEC_GETSIMLOCKSTATUS)
-(
-	IN __int64 val1
-);
-typedef NPETSEC_GETSIMLOCKSTATUS FAR* LPNPETSEC_GETSIMLOCKSTATUS;
-
-typedef void(STDAPICALLTYPE NPETSEC_DEINIT)
-(
-	
-);
-typedef NPETSEC_DEINIT FAR* LPNPETSEC_DEINIT;
