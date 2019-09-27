3d1f17024b69fb16528d1dc342e54b38d71c9e57
Gus <gus33000@live.fr>
2019-09-27 09:29:46 +0200

    Added more stub functions

--- NPETSEC.cpp
+++ NPETSEC.cpp
@@ -236,3 +236,227 @@ void    NPETSEC_Deinit()
 {
 	
 }
+
+//
+//
+//
+
+__int64 NPETSEC_DeleteSecurityLogFile(unsigned int a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetConfigAccessMode(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetProductionData(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetSecurityLogFileSize(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetSecurityMode(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_GetUefiCertStates(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_IsProductionDone(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_LastErrorStr()
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcEfsDataFileWrite(__int64 a1, __int64 a2, unsigned int a3)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcEfsFileDelete(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcEfsFileRead(__int64 a1, __int64 a2, __int64 a3)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcEfsFileWrite(__int64 a1, __int64 a2, unsigned int a3)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcEfsItemFileWrite(__int64 a1, __int64 a2, unsigned int a3)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcNvItemIndexedRead(unsigned __int16 a1, unsigned __int16 a2, unsigned __int8 a3, __int64 a4, __int64 a5)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcNvItemWrite(unsigned __int16 a1, unsigned __int16 a2, __int64 a3, unsigned int a4)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcNvSpcUnlock(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcSubscriptionNvItemIndexedRead(unsigned __int16 a1, __int64 a2, unsigned __int8 a3, __int64 a4, __int64 a5)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcSubscriptionNvItemRead(unsigned __int16 a1, __int64 a2, __int64 a3, __int64 a4)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QcSubscriptionNvItemWrite(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_QfsEfsFileWrite(unsigned __int16 a1, __int64 a2, __int64 a3, unsigned int a4)
+{
+	return 0;
+}
+
+__int64 NPETSEC_RdcPhoneReq(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadBtId(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadImeiRaw(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadImeiStr(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadImeiStrEx(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadManufacturingData(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadMeidRaw(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadMeidStr(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadPublicId(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadSecFea(__int64 a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadSecFeaSize(__int64 a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadSecurityLogFile(unsigned int a1, __int64 a2, __int64 a3, unsigned int a4, __int64 a5)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadServiceTag(__int64 a1, unsigned int a2, __int64 a3, __int64 a5)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadServiceTag3(__int64 a1, unsigned int a2, __int64 a3, __int64 a5)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadServiceTag3Size(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadServiceTagSize(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_ReadWlanId(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_RebootDevice(unsigned int a1, unsigned int a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_RegItemValueWrite(unsigned int a1, __int64 a2, unsigned int a3)
+{
+	return 0;
+}
+
+__int64 NPETSEC_SetConfigAccessMode(unsigned int a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_SetConfigAccessModeExternal(unsigned int a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_SetDriverLogging(unsigned int a1)
+{
+	return 0;
+}
+
+__int64 NPETSEC_SetProductionDataUnchecked(unsigned int a1, __int64 a2)
+{
+	return 0;
+}
+
+__int64 NPETSEC_SetSecurityMode(unsigned int a1)
+{
+	return 0;
+}
